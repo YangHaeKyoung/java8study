f5f5d2214da3a6607c191b156904176b71356e82
baguni <eunhee.park@navercorp.com>
2016-11-16 17:21:35 +0900

    일단 commit test도 좀 잘 해봐야겠네

--- src/main/java/com/navercorp/park/chapter/two/Apple.java
+++ src/main/java/com/navercorp/park/chapter/two/Apple.java
@@ -16,4 +16,7 @@ public class Apple {
 	public String getColor(){
 		return color;
 	}
+	public void setColor(String color){
+		this.color = color;
+	}
 }
--- src/test/java/com/navercorp/park/chapter/two/AppleFilterTest.java
+++ src/test/java/com/navercorp/park/chapter/two/AppleFilterTest.java
@@ -7,9 +7,47 @@
 
 package com.navercorp.park.chapter.two;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * @author Naver
  */
 public class AppleFilterTest {
+
+	/**
+	 * Test method for {@link com.navercorp.park.chapter.two.AppleFilter#fillterGreenAppples(java.util.List)}.
+	 */
+	@Test
+	public void testFillterGreenAppples() {
+		
+		List<Apple> inventory = AppleFilterTest.returnAppleListByColor(new String[]{"green","green","red","green","green","blue","green","green","black","yellow","green","green","green","green"});
+		List<Apple> appleResultList = AppleFilterTest.returnAppleListByColor(new String[]{"green","green","green","green","green","green","green","green","green","green"});
+		List<Apple> resultList = AppleFilter.fillterGreenAppples(inventory);
+		
+		Assert.assertArrayEquals(resultList.toArray(), appleResultList.toArray());
+		//assetEquals
+		//assetArrayEqulas
+		//assetSame()//동일 객체 확인
+		//assetTrue(a)
+		//assetNotNull(a)
+	}
 	
+	static List<Apple> returnAppleListByColor(String[] colorArry){
+		List<Apple> list = new ArrayList<Apple>();
+		for( String color : colorArry){
+			Apple e = new Apple();
+			e.setColor(color);
+			list.add(e);
+		}
+		return list;
+		
+	}	
+	//객체초기화 하는 작업
+	//@Before 
+	
+	//@After 테스트후 실행
 }
